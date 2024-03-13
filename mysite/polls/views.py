9bd2fbd5b0e8bde36b25e320b5a37a09519ea0b5
yanguoqiang <yanguoqiang@yiritech.com>
2024-03-13 17:28:11 +0800

    利用django.views.generic生成view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 from .models import Question,Choice
@@ -5,8 +6,9 @@ from django.db.models import F
 from django.template import loader
 from django.shortcuts import render
 from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+#手写生成view方法
 def index(request):
   latest_question_list = Question.objects.order_by("-pub_date")[:5]
   context = {
@@ -62,3 +64,18 @@ def vote(request, question_id):
     
 
 
+#定义modelView类，简化代码
+class IndexView(generic.ListView):
+   template_name = "polls/index.html"
+   context_object_name = "latest_question_list"
+
+   def get_queryset(self):
+      return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+   model = Question
+   template_name = "polls/detail.html"
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
